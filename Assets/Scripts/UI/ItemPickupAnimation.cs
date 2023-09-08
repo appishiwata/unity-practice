dc567b0cd3378fbf57a2d001140bd8cd1c99fdf5
appishiwata <app.ishiwata@gmail.com>
2023-09-08 09:21:54 +0900

    スクリプトファイルのwarning対応

--- Assets/Scripts/UI/ItemPickupAnimation.cs
+++ Assets/Scripts/UI/ItemPickupAnimation.cs
@@ -1,18 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemPickupAnimation : MonoBehaviour
+namespace UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ItemPickupAnimation : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
