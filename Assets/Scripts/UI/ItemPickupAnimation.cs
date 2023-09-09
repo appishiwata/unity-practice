3511154f8b7622f5682ee5da20c679dc99b1c1d1
appishiwata <app.ishiwata@gmail.com>
2023-09-09 10:55:35 +0900

    アイテム取得アニメーション作成

--- Assets/Scripts/UI/ItemPickupAnimation.cs
+++ Assets/Scripts/UI/ItemPickupAnimation.cs
@@ -1,8 +1,33 @@
+using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 namespace UI
 {
     public class ItemPickupAnimation : MonoBehaviour
     {
+        [SerializeField] private RectTransform _prefab;
+        [SerializeField] private RectTransform _target;
+        [SerializeField] private Canvas _canvas;
+        [SerializeField] private float _duration = 1f;
+        [SerializeField] private int _copies = 10;
+
+        public void AnimateItemPickup()
+        {
+            StartCoroutine(Animate());
+        }
+
+        private IEnumerator Animate()
+        {
+            for (int i = 0; i < _copies; i++)
+            {
+                var instance = Instantiate(_prefab, _canvas.transform);
+                instance.transform.DOMove(_target.position, _duration).SetEase(Ease.InOutQuad).OnComplete(() =>
+                {
+                    Destroy(instance.gameObject);
+                });
+                yield return new WaitForSeconds(_duration / _copies);
+            }
+        }
     }
 }
