d2192c3bea1d5e679a8e4e891089d6b07388fadd
appishiwata <app.ishiwata@gmail.com>
2023-09-10 11:55:44 +0900

    アニメーション処理UniTask版を追加

--- Assets/Scripts/UI/ItemPickupAnimation.cs
+++ Assets/Scripts/UI/ItemPickupAnimation.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using DG.Tweening;
 
@@ -11,10 +14,15 @@ namespace UI
         [SerializeField] private Canvas _canvas;
         [SerializeField] private float _duration = 1f;
         [SerializeField] private int _copies = 10;
+        
+        [SerializeField] RectTransform _start;
+        [SerializeField] RectTransform _end;
+        [SerializeField] GameObject _item;
 
-        public void AnimateItemPickup()
+        public async void AnimateItemPickup()
         {
-            StartCoroutine(Animate());
+            await ItemAnimation(_canvas, _start, _end, _item, 0.2f, 1f, 10);
+            //StartCoroutine(Animate());
         }
 
         private IEnumerator Animate()
@@ -27,6 +35,23 @@ namespace UI
                     Destroy(instance.gameObject);
                 });
                 yield return new WaitForSeconds(_duration / _copies);
+            }
+        }
+        
+        private async Task ItemAnimation(Canvas canvas, RectTransform start, RectTransform end, GameObject prefab, float initialDelay, float duration, int copies)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(initialDelay));
+            for (int i = 0; i < copies; i++)
+            {
+                var obj = Instantiate(prefab, canvas.transform);
+                obj.transform.position = start.position;
+                obj.transform.DOMove(end.position, duration)
+                    .SetEase(Ease.InOutQuad)
+                    .OnComplete(() =>
+                    {
+                        Destroy(obj);
+                    });
+                await UniTask.Delay(TimeSpan.FromSeconds(duration / copies));
             }
         }
     }
