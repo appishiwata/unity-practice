26f7e7e5d37243b0ca42cd6b455a5a92b80c206c
appishiwata <app.ishiwata@gmail.com>
2023-09-11 07:03:23 +0900

    ItemAnimationHelperを作成して処理を分離

---
+++ Assets/Scripts/UI/ItemAnimationHelper.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+
+namespace UI
+{
+    public class AnimationParameter
+    {
+        public Canvas Canvas { get; set; }
+        public RectTransform Start { get; set; }
+        public RectTransform End { get; set; }
+        public GameObject Prefab { get; set; }
+        public float InitialDelay { get; set; } = 0.2f;
+        public float Duration { get; set; } = 1.0f;
+        public int Copies { get; set; } = 10;
+    }
+
+    [Serializable]
+    public class RectTransformPair
+    {
+        public RectTransform _start;
+        public RectTransform _end;
+    }
+
+    public class ItemAnimationHelper
+    {
+        private readonly Func<GameObject, Transform, GameObject> _instantiateFunc;
+        private readonly Action<GameObject> _destroyAction;
+
+        public ItemAnimationHelper(Func<GameObject, Transform, GameObject> instantiateFunc, Action<GameObject> destroyAction)
+        {
+            _instantiateFunc = instantiateFunc;
+            _destroyAction = destroyAction;
+        }
+
+        public async Task AnimateItemPickup(AnimationParameter param)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(param.InitialDelay));
+            for (int i = 0; i < param.Copies; i++)
+            {
+                var obj = _instantiateFunc(param.Prefab, param.Canvas.transform);
+                obj.transform.position = param.Start.position;
+                obj.transform.DOMove(param.End.position, param.Duration)
+                    .SetEase(Ease.InOutQuad)
+                    .OnComplete(() =>
+                    {
+                        _destroyAction(obj);
+                    });
+                await UniTask.Delay(TimeSpan.FromSeconds(param.Duration / param.Copies));
+            }
+        }
+    }
+}
--- Assets/Scripts/UI/ItemPickupAnimation.cs
+++ Assets/Scripts/UI/ItemPickupAnimation.cs
@@ -1,67 +1,30 @@
-using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Cysharp.Threading.Tasks;
 using UnityEngine;
-using DG.Tweening;
 
 namespace UI
 {
-    public class AnimationParameter
-    {
-        public Canvas Canvas { get; set; }
-        public RectTransform Start { get; set; }
-        public RectTransform End { get; set; }
-        public GameObject Prefab { get; set; }
-
-        public float InitialDelay { get; set; } = 0.2f;
-        public float Duration { get; set; } = 1.0f;
-        public int Copies { get; set; } = 10;
-    }
-
     public class ItemPickupAnimation : MonoBehaviour
     {
         [SerializeField] Canvas _canvas;
         [SerializeField] GameObject _prefab;
-
-        [Serializable]
-        public class RectTransformPair
-        {
-            public RectTransform _start;
-            public RectTransform _end;
-        }
         [SerializeField] List<RectTransformPair> _rectTransformPairs;
 
-        public async void AnimateItemPickup()
+        private ItemAnimationHelper _helper;
+
+        async void Start()
         {
+            _helper = new ItemAnimationHelper(Instantiate, Destroy);
             foreach (var t in _rectTransformPairs)
             {
-                var animationParameter = new AnimationParameter
+                AnimationParameter param = new AnimationParameter
                 {
                     Canvas = _canvas,
                     Start = t._start,
                     End = t._end,
                     Prefab = _prefab
                 };
-
-                await AnimateItemPickup(animationParameter);
-            }
-        }
-
-        private async Task AnimateItemPickup(AnimationParameter param)
-        {
-            await UniTask.Delay(TimeSpan.FromSeconds(param.InitialDelay));
-            for (int i = 0; i < param.Copies; i++)
-            {
-                var obj = Instantiate(param.Prefab, param.Canvas.transform);
-                obj.transform.position = param.Start.position;
-                obj.transform.DOMove(param.End.position, param.Duration)
-                    .SetEase(Ease.InOutQuad)
-                    .OnComplete(() =>
-                    {
-                        Destroy(obj);
-                    });
-                await UniTask.Delay(TimeSpan.FromSeconds(param.Duration / param.Copies));
+                
+                await _helper.AnimateItemPickup(param);
             }
         }
     }
