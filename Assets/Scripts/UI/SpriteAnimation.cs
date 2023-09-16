612810cdce2fdc8f64a8fb99409929a21a60541e
appishiwata <app.ishiwata@gmail.com>
2023-09-16 23:48:29 +0900

    屋根開閉をボタンで出来るように

--- Assets/Scripts/UI/SpriteAnimation.cs
+++ Assets/Scripts/UI/SpriteAnimation.cs
@@ -9,11 +9,25 @@ namespace UI
         [SerializeField] Animator _animator;
         [SerializeField] Slider _slider;
         
+        [SerializeField] Animator _roofAnimator;
+        [SerializeField] Button _roofOpenButton;
+        [SerializeField] Button _roofCloseButton;
+        
         void Start()
         {
             _slider.ObserveEveryValueChanged(slider => slider.value)
                 .Subscribe(value => _animator.speed = value)
                 .AddTo(this);
+
+            _roofOpenButton.OnClickAsObservable()
+                .Subscribe(_ => _roofAnimator.SetBool("IsOpen", true))
+                .AddTo(this);
+
+            _roofCloseButton.OnClickAsObservable()
+                .Subscribe(_ => _roofAnimator.SetBool("IsOpen", false))
+                .AddTo(this);
+            
+            // TODO 最初に一度だけアニメーションが実行されてしまう。
         }
     }
 }
