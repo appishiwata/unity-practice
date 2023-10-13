f154d0bb9ef3e7c133e11976eee3751ff09ff8c6
appishiwata <app.ishiwata@gmail.com>
2023-10-14 05:30:27 +0900

    無限スクロール マテリアル使用ver

--- Assets/Scripts/UI/BackgroundScroll.cs
+++ Assets/Scripts/UI/BackgroundScroll.cs
@@ -9,6 +9,7 @@ public class BackgroundScroll : MonoBehaviour
     [SerializeField] Image _bgYokonaga;
     [SerializeField] Image _bgTatenaga;
     [SerializeField] Image _bgRepeat;
+    [SerializeField] Image _bgRepeatMaterial;
     
     void Start()
     {
@@ -28,6 +29,15 @@ public class BackgroundScroll : MonoBehaviour
         // 画像自体を動かす 端まで行ったら戻る設定
         // 画像をTiledに設定、Widthを2倍にして、もとのWidth分画像を左に配置、そのWidthの半分動かす
         _bgRepeat.transform.DOLocalMoveX(-540f, 10f)
+            .SetLoops(-1, LoopType.Restart)
+            .SetEase(Ease.Linear);
+        
+        // 縦長画像 + マテリアルを使用
+        // マテリアルのオフセットを動かす
+        // 画像はSimple, SpriteのWrapModeをRepeatに設定
+        // マテリアルはUnlit/Transparent
+        _bgRepeatMaterial.material.DOOffset(Vector2.zero, 0f);
+        _bgRepeatMaterial.material.DOOffset(new Vector2(1f,0f), 10f)
             .SetLoops(-1, LoopType.Restart)
             .SetEase(Ease.Linear);
     }
