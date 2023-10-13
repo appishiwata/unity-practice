86c6c918b626cf1bad0536fac4df28b9b1f8fbc7
appishiwata <app.ishiwata@gmail.com>
2023-10-13 09:52:54 +0900

    縦長の画像スクロール restart形式に

--- Assets/Scripts/UI/BackgroundScroll.cs
+++ Assets/Scripts/UI/BackgroundScroll.cs
@@ -6,13 +6,21 @@ using UnityEngine.UI;
 
 public class BackgroundScroll : MonoBehaviour
 {
-    [SerializeField] Image _backgroundImage;
+    [SerializeField] Image _bgYokonaga;
+    [SerializeField] Image _bgTatenaga;
+    
     void Start()
     {
         // 横長画像
         // 画像自体を動かす 往復する設定
-        _backgroundImage.transform.DOLocalMoveX(-540f, 10f)
+        _bgYokonaga.transform.DOLocalMoveX(-540f, 10f)
             .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.Linear);
+        
+        // 縦長画像
+        // 画像自体を動かす 端まで行ったら戻る設定
+        _bgTatenaga.transform.DOLocalMoveX(-400f, 10f)
+            .SetLoops(-1, LoopType.Restart)
             .SetEase(Ease.Linear);
     }
 }
